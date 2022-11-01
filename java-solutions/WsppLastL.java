3d50cb522145b96780a7a828836b1a1d07ded3c9
Nikolai Korobeinikov <ricnorr@yandex-team.ru>
2022-11-01 12:59:22 +0300

    Checked by an instructor

--- java-solutions/WsppLastL.java
+++ java-solutions/WsppLastL.java
@@ -55,33 +55,34 @@ public class WsppLastL {
 }
 
 /**
- * Exception in thread "main" java.lang.AssertionError: Line 8:
- *      expected `question 1 10`,
- *        actual `question 1 1`
- *         at base.Asserts.error(Asserts.java:75)
- *         at base.Asserts.assertTrue(Asserts.java:41)
- *         at base.Asserts.assertEquals(Asserts.java:20)
- *         at base.Runner.lambda$testEquals$0(Runner.java:36)
- *         at base.TestCounter.lambda$test$0(TestCounter.java:58)
- *         at base.TestCounter.lambda$testV$2(TestCounter.java:71)
- *         at base.Log.silentScope(Log.java:40)
- *         at base.TestCounter.testV(TestCounter.java:70)
- *         at base.TestCounter.test(TestCounter.java:57)
- *         at base.Runner.testEquals(Runner.java:30)
- *         at base.MainChecker.testEquals(MainChecker.java:25)
- *         at wordStat.WordStatChecker.test(WordStatChecker.java:66)
- *         at wordStat.WordStatChecker.test(WordStatChecker.java:49)
- *         at wspp.WsppTester.lambda$variant$9(WsppTester.java:76)
- *         at wordStat.WordStatChecker.test(WordStatChecker.java:44)
- *         at wspp.WsppTester.lambda$variant$10(WsppTester.java:23)
- *         at base.Selector.lambda$test$2(Selector.java:79)
- *         at base.Log.lambda$action$0(Log.java:18)
- *         at base.Log.silentScope(Log.java:40)
- *         at base.Log.scope(Log.java:31)
- *         at base.Log.scope(Log.java:24)
- *         at base.Selector.lambda$test$3(Selector.java:79)
- *         at java.base/java.util.ArrayList.forEach(ArrayList.java:1511)
- *         at base.Selector.test(Selector.java:79)
- *         at base.Selector.main(Selector.java:51)
- *         at wspp.FullWsppTest.main(FullWsppTest.java:57)
+ Exception in thread "main" java.lang.AssertionError: Line 8:
+ expected `question 1 10`,
+ actual `question 1 1`
+ at base.Asserts.error(Asserts.java:75)
+ at base.Asserts.assertTrue(Asserts.java:41)
+ at base.Asserts.assertEquals(Asserts.java:20)
+ at base.Runner.lambda$testEquals$0(Runner.java:36)
+ at base.TestCounter.lambda$test$0(TestCounter.java:58)
+ at base.TestCounter.lambda$testV$2(TestCounter.java:71)
+ at base.Log.silentScope(Log.java:40)
+ at base.TestCounter.testV(TestCounter.java:70)
+ at base.TestCounter.test(TestCounter.java:57)
+ at base.Runner.testEquals(Runner.java:30)
+ at base.MainChecker.testEquals(MainChecker.java:25)
+ at wordStat.WordStatChecker.test(WordStatChecker.java:66)
+ at wordStat.WordStatChecker.test(WordStatChecker.java:49)
+ at wspp.WsppTester.lambda$variant$9(WsppTester.java:76)
+ at wordStat.WordStatChecker.test(WordStatChecker.java:44)
+ at wspp.WsppTester.lambda$variant$10(WsppTester.java:23)
+ at base.Selector.lambda$test$2(Selector.java:79)
+ at base.Log.lambda$action$0(Log.java:18)
+ at base.Log.silentScope(Log.java:40)
+ at base.Log.scope(Log.java:31)
+ at base.Log.scope(Log.java:24)
+ at base.Selector.lambda$test$3(Selector.java:79)
+ at java.base/java.util.ArrayList.forEach(ArrayList.java:1511)
+ at base.Selector.test(Selector.java:79)
+ at base.Selector.main(Selector.java:51)
+ at wspp.FullWsppTest.main(FullWsppTest.java:57)
+
  */
