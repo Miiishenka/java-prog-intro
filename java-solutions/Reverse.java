956ff1ce90acc982af5fd35f273fbc09b818a9a7
CHelebaev_Mikhail <367611@niutmo.ru>
2022-12-07 13:07:33 +0300

    Reverse

--- java-solutions/Reverse.java
+++ java-solutions/Reverse.java
@@ -1,37 +1,38 @@
 import java.util.Arrays;
-import java.io.*;
+import java.util.Scanner;
 public class Reverse {
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		String[][] matrix = new String[1][];
+		Scanner sc = new Scanner(System.in);
+		int[][] matr = new int[1][];
 		int[] lineLens = new int[1];
-		String[] arr = new String[1];
-		int k = 0;
-		int line = 0;
-		int countStr = 1;
-		while (scanner.hasNext()) {
-			String number = scanner.next();
-			while (countStr != scanner.count) {
-				if (line == matrix.length) {
-					matrix = Arrays.copyOf(matrix, line * 2);
-					lineLens = Arrays.copyOf(lineLens, line * 2);
+		int m = 0;
+		int maxLine = 0;
+		while (sc.hasNextLine()) {
+			String str = sc.nextLine();
+			Scanner s = new Scanner(str);
+			int k = 0;
+			int[] arr = new int[1];
+			while (s.hasNextInt()) {
+				if (k == arr.length) {
+					arr = Arrays.copyOf(arr,k*3/2+1);
 				}
-				matrix[line] = arr;
-				lineLens[line] = k;
-				line++;
-				arr = Arrays.copyOf(arr, 0);
-				k = 0;
-				countStr++;
+				arr[k] = s.nextInt();
+				k++;
+			}				
+			if (m == matr.length) {
+				matr = Arrays.copyOf(matr,m*3/2+1);
+				lineLens = Arrays.copyOf(lineLens,m*3/2+1);
 			}
-			if (k == arr.length) {
-					arr = Arrays.copyOf(arr, k * 2 + 1);
-				}
-			arr[k++] = number;			
+			matr[m] = arr;
+			lineLens[m] = k;			
+			m++;
+			if (k > maxLine) {
+				maxLine = k;
+			}
 		}
-		scanner.close();
-		for (int i = line - 1; i >= 0; i--) {
+		for (int i = m - 1; i >= 0; i--) {
 			for (int j = lineLens[i] - 1; j > -1; j--) {
-				System.out.print(matrix[i][j] + " ");
+				System.out.print(matr[i][j] + " ");
 			}
 			System.out.println();
 		}
