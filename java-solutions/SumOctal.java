23648728dede84a58ae53d0ce852d1f588f6c341
CHelebaev_Mikhail <367611@niutmo.ru>
2022-12-07 12:44:08 +0300

    Sum

--- java-solutions/SumOctal.java
+++ java-solutions/SumOctal.java
@@ -1,34 +1,3 @@
-/*public class SumOctal {
-    public static void main(String[] args) {
-	    int ans = 0;		
-		for (int i = 0; i < args.length; i++) {
-			boolean started = false;
-			int radix = 10;
-			int beginning = 0;
-			int end = 0;
-            for (int j = 0; j < args[i].length(); j++) {
-                if (!(Character.isWhitespace(args[i].charAt(j))) && (j != args[i].length() - 1)) {
-                    if (!started) {
-						started = true;
-						beginning = j;
-                    } else {
-						if (args[i].charAt(j) == 'O' || args[i].charAt(j) == 'o') {
-							radix = 8;
-						}
-					}
-				} else {
-					if (started) {
-						ans += Integer.parseInt(args[i].substring(beginning, end + 1), radix);
-					}
-					radix = 10;
-					started = false;
-				}
-			}			
-		}				
-		System.out.println(ans);
-	}
-}
-*/
 public class SumOctal {
     public static void main(String[] args) {
 	    int ans = 0;		
@@ -36,11 +5,11 @@ public class SumOctal {
 			for(int k = 0; k < args[i].length(); k++) {
 				if(!(Character.isWhitespace(args[i].charAt(k)))){
 					int start = k;
-					while ((k<args[i].length()) && !(Character.isWhitespace(args[i].charAt(k)))) {
+					while ((k < args[i].length()) && !(Character.isWhitespace(args[i].charAt(k)))) {
 						k++;
 					}
 					if (args[i].charAt(k-1) == 'O' || args[i].charAt(k-1) == 'o') {
-						ans += Integer.parseInt(args[i].substring(start, k-1), 8);
+						ans += Integer.parseUnsignedInt(args[i].substring(start, k - 1), 8);
 					} else {
 						ans += Integer.parseInt(args[i].substring(start, k));
 					}
