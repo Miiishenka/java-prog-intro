37f12b1f5f761dcac400dc0534eac4509b2fc0ae
CHelebaev_Mikhail <367611@niutmo.ru>
2022-12-20 20:33:12 +0300

    scan

--- java-solutions/ReverseOctDec.java
+++ java-solutions/ReverseOctDec.java
@@ -1,39 +1,24 @@
-import java.util.Arrays;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class ReverseOctDec {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		Scanner scanner = new Scanner(System.in);
-		String[][] matrix = new String[1][];
-		int[] lineLens = new int[1];
-		String[] arr = new String[1];
-		int k = 0;
-		int line = 0;
-		int countStr = 1;
+		List<List<Integer>> matrix = new ArrayList<>();
 		while (scanner.hasNext()) {
-			String number = scanner.next();
-			while (countStr != scanner.count) {
-				if (line == matrix.length) {
-					matrix = Arrays.copyOf(matrix, line * 2);
-					lineLens = Arrays.copyOf(lineLens, line * 2);
-				}
-				matrix[line] = arr;
-				lineLens[line] = k;
-				line++;
-				arr = Arrays.copyOf(arr, 0);
-				k = 0;
-				countStr++;
+			List<Integer> list = scanner.nextLine();
+			if (list == null) {
+				break;
 			}
-			if (k == arr.length) {
-					arr = Arrays.copyOf(arr, k * 2 + 1);
-				}
-			if (number.length() >= 1 && Character.toLowerCase(number.charAt(number.length() - 1)) == 'o') {
-				number = Integer.toString(Integer.parseUnsignedInt(number.substring(0, number.length() - 1), 8));
-			}
-			arr[k++] = number;
+			Collections.reverse(list);
+			matrix.add(list);
 		}
-		scanner.close();
-		for (int i = line - 1; i >= 0; i--) {
-			for (int j = lineLens[i] - 1; j > -1; j--) {
-				System.out.print(matrix[i][j] + " ");
+		Collections.reverse(matrix);
+		for (List<Integer> list : matrix) {
+			for (Integer x : list) {
+				System.out.print(x + " ");
 			}
 			System.out.println();
 		}
--- java-solutions/Scanner.java
+++ java-solutions/Scanner.java
@@ -5,47 +5,49 @@ class Scanner {
 	StringBuilder token = new StringBuilder();
 	int index = -1;
 	int count = 1;
-	Reader reader;
+	BufferedReader reader;
 	int read;
 	int wordIndex = 0;
 	StringBuilder wordToken = new StringBuilder();
 	StringBuilder word = new StringBuilder();
 	Scanner(InputStream inputStream) {
-		this.reader = new InputStreamReader(inputStream);
-		try {
-			read = reader.read(buffer);
-		} catch (IOException e) {
-            System.out.println("Input error: " + e.getMessage());
-        }
-	}
-	Scanner(String file) {
-		try {
-			this.reader = new InputStreamReader(
-					new FileInputStream(file),
-					"UTF8"
-				);;
-		} catch (IOException e) {
-            System.out.println("Input file reading error: " + e.getMessage());
-		}
-			
-		try {
-			read = reader.read(buffer);
-		} catch (IOException e) {
-            System.out.println("Input file reading error: " + e.getMessage());
-        }
+		this.reader = new BufferedReader(new InputStreamReader(inputStream));
 	}
 	String next() {
 		token.setLength(0);
 		while (this.hasNext()) {
 			for (int i = index + 1; i < buffer.length; i++) {
-				if (buffer[i] == '\n' || Character.isWhitespace(buffer[i]) || buffer[i] == '\0') {
-					if (buffer[i] == '\n') {
-						count++;
+				if (Character.isWhitespace(buffer[i]) || buffer[i] == '\0') {
+					if (token.length() >= 1) {
+						index = i;
+						return new String(token);
+					}
+				} else if (buffer[i] == '\n' || buffer[i] == '\u000C' || buffer[i] == '\u0085' ||
+						buffer[i] == '\u2029' || buffer[i] == '\u000B' || buffer[i] == '\u2028') {
+					System.out.println("n");
+					count++;
+					if (token.length() >= 1) {
+						index = i;
+						return new String(token);
+					}
+				} else if (buffer[i] == '\r') {
+					System.out.println("r");
+					count++;
+					if (i == buffer.length - 1) {
+						try {
+							read = reader.read(buffer);
+							i = 0;
+						} catch (IOException e) {
+							System.out.println("Input file reading error: " + e.getMessage());
+						}
+					}
+					if (buffer[i + 1] == '\n') {
+						i++;
 					}
 					if (token.length() >= 1) {
 						index = i;
 						return new String(token);
-					}				
+					}
 				} else {
 					token.append(buffer[i]);
 				}
@@ -91,6 +93,40 @@ class Scanner {
 			reader.close();
 		} catch (IOException e) {
             System.out.println("Scanner close error" + e.getMessage());
+		}
+	}
+	List<Integer> nextLine() throws IOException {
+		try {
+			List<Integer> list = new ArrayList<>();
+			String s = reader.readLine();
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < s.length(); i++) {
+				if (Character.isWhitespace(s.charAt(i))) {
+					if (sb.length() > 0) {
+						if (sb.charAt(sb.length() - 1) == 'o' || sb.charAt(sb.length() - 1) == 'O') {
+							list.add(Integer.parseUnsignedInt(sb.substring(0, sb.length() - 1), 8));
+						} else {
+							list.add(Integer.parseInt(sb.toString()));
+						}
+						sb.setLength(0);
+					}
+				} else {
+					sb.append(s.charAt(i));
+					if (i == s.length() - 1) {
+						if (sb.length() > 0) {
+							if (sb.charAt(sb.length() - 1) == 'o' || sb.charAt(sb.length() - 1) == 'O') {
+								list.add(Integer.parseUnsignedInt(sb.substring(0, sb.length() - 1), 8));
+							} else {
+								list.add(Integer.parseInt(sb.toString()));
+							}
+							sb.setLength(0);
+						}
+					}
+				}
+			}
+			return list;
+		} catch (NullPointerException e) {
+			return null;
 		}
 	}
 }
